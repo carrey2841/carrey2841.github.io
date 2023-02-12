d1c855d947ead8793ca6b5c7fd8cab94cd89dbd3
이재훈 <jaehun.lee@woowahan.com>
2023-02-13 02:14:51 +0900

    수리성명학 로직 추가

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,5 +1,5 @@
 import { FC, Suspense } from 'react'
-import {HashRouter, Route, Routes} from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import NotFound from '~/components/NotFound'
 import PagesCarNumbers from './carnumbers'
 
