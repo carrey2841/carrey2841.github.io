6c463b64c1d9b97946166fe24029b23531d3caed
이재훈 <jaehun.lee@woowahan.com>
2023-02-13 00:40:10 +0900

    hash 라우터로 변경

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,16 +1,16 @@
 import { FC, Suspense } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {HashRouter, Route, Routes} from 'react-router-dom'
 import NotFound from '~/components/NotFound'
 import PagesCarNumbers from './carnumbers'
 
 const PagesRouter: FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/car-numbers" element={<PagesCarNumbers />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
